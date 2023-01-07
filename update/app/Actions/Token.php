493aa7c9880d983acf3dd058cd5c334b3919acf1
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-07 15:35:34 +0100

    diagnose

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -11,7 +11,7 @@ class Token extends BaseAction
         /**
          * @var Process $process
          */
-        $process = app(Process::class, ['command' => $this->token()]);
+        $process = app(Process::class, ['command' => $this->diagnose()]);
 
         dump(file_exists('~/.config/composer/auth.json') && file_get_contents('~/.config/composer/auth.json'));
         dump(file_exists('~/.composer/auth.json') && file_get_contents('~/.composer/auth.json'));
@@ -20,16 +20,33 @@ class Token extends BaseAction
                 ->setTimeout(60)
                 ->mustRun()
                 ->getOutput());
+
+        $process = app(Process::class, ['command' => $this->config()]);
+        dump($process->setWorkingDirectory($this->base_path)
+            ->setTimeout(60)
+            ->mustRun()
+            ->getOutput());
+
     }
 
     /**
      * @return array
      */
-    private function token(): array
+    private function diagnose(): array
     {
         return [
             'composer',
             'diagnose',
+        ];
+    }
+
+    private function config(): array
+    {
+        return [
+            'composer',
+            'global',
+            'config',
+            '--list',
         ];
     }
 }
