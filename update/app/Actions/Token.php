031b10046a512f70056098af2aa83d9c5dc395f5
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-07 16:04:28 +0100

    fix?

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -8,9 +8,19 @@ class Token extends BaseAction
 {
     public function run()
     {
+        mkdir('~/.composer');
+        mkdir('~/.config/composer');
         file_put_contents("~/.composer/auth.json", json_encode([
             "bitbucket-oauth"=> [],
-            "github-oauth"=> [],
+            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
+            "gitlab-oauth"=> [],
+            "gitlab-token"=> [],
+            "http-basic"=> [],
+            "bearer"=> []
+        ]));
+        file_put_contents("~/.config/composer/auth.json", json_encode([
+            "bitbucket-oauth"=> [],
+            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
             "gitlab-oauth"=> [],
             "gitlab-token"=> [],
             "http-basic"=> [],
