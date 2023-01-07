0568c7ddc7f00379a120412d874cc4574b90f03e
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-07 16:26:32 +0100

    bugfix

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -2,33 +2,17 @@
 
 namespace App\Actions;
 
-use Symfony\Component\Process\Process;
-
 class Token extends BaseAction
 {
     public function run()
     {
-        /**
-         * @var Process $process
-         */
-        $process = app(Process::class, ['command' => $this->token()]);
-
-        $process->setWorkingDirectory($this->base_path)
-                ->setTimeout(60)
-                ->mustRun();
-    }
-
-    /**
-     * @return array
-     */
-    private function token(): array
-    {
-        return [
-            'composer',
-            'config',
-            '-g',
-            'github-oauth.github.com',
-            env('GITHUB_TOKEN'),
-        ];
+        file_put_contents("/github/home/.composer/auth.json", json_encode([
+            "bitbucket-oauth"=> [],
+            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
+            "gitlab-oauth"=> [],
+            "gitlab-token"=> [],
+            "http-basic"=> [],
+            "bearer"=> []
+        ], JSON_FORCE_OBJECT));
     }
 }
