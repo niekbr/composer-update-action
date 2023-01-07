ace6907b7fa10f5155e923478f1091e48d68df03
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-07 15:31:57 +0100

    debug

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -13,6 +13,9 @@ class Token extends BaseAction
          */
         $process = app(Process::class, ['command' => $this->token()]);
 
+        dump(file_exists('~/.config/composer/auth.json') && file_get_contents('~/.config/composer/auth.json'));
+        dump(file_exists('~/.composer/auth.json') && file_get_contents('~/.composer/auth.json'));
+
         dump($process->setWorkingDirectory($this->base_path)
                 ->setTimeout(60)
                 ->mustRun()
