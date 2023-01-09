732f750db6823f44c541196ebdde6c9e6e058d8a
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-09 20:14:44 +0100

    bugfix

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -6,6 +6,10 @@ class Token extends BaseAction
 {
     public function run()
     {
+        if (! file_exists('/github/home/.composer')) {
+            mkdir('/github/home/.composer');
+        }
+
         file_put_contents("/github/home/.composer/auth.json", json_encode([
             "bitbucket-oauth"=> [],
             "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
