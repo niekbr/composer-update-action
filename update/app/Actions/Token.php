c6e45e697a3c85decf9494b5502ec80d0314adf1
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-10 20:06:57 +0100

    debug

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -9,18 +9,22 @@ class Token extends BaseAction
         $this->execDump('ls -al /github/home/.composer');
         $this->execDump('cat /github/home/.composer/auth.json');
 
+        dump(env('GITHUB_TOKEN'));
+
 //        if (! file_exists(env('COMPOSER_HOME'))) {
 //            mkdir(env('COMPOSER_HOME'));
 //        }
 //
-//        file_put_contents(env('COMPOSER_HOME')."/auth.json", json_encode([
-//            "bitbucket-oauth"=> [],
-//            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
-//            "gitlab-oauth"=> [],
-//            "gitlab-token"=> [],
-//            "http-basic"=> [],
-//            "bearer"=> []
-//        ], JSON_FORCE_OBJECT));
+        file_put_contents('/github/home/.composer/auth.json', json_encode([
+            "bitbucket-oauth"=> [],
+            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
+            "gitlab-oauth"=> [],
+            "gitlab-token"=> [],
+            "http-basic"=> [],
+            "bearer"=> []
+        ], JSON_FORCE_OBJECT));
+
+        $this->execDump('cat /github/home/.composer/auth.json');
     }
 
     protected function execDump($command)
