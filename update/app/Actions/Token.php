476a527eb31b3a9d9892305fc5530234dc150baa
Niek Brekelmans <niek.brekelmans@gmail.com>
2023-01-10 20:04:13 +0100

    try

--- update/app/Actions/Token.php
+++ update/app/Actions/Token.php
@@ -6,22 +6,21 @@ class Token extends BaseAction
 {
     public function run()
     {
-        $this->execDump('export COMPOSER_HOME="$HOME/.composer"');
         $this->execDump('ls -al /github/home/.composer');
-        $this->execDump('ls -al /github/home/.config/composer');
+        $this->execDump('cat /github/home/.composer/auth.json');
 
-        if (! file_exists(env('COMPOSER_HOME'))) {
-            mkdir(env('COMPOSER_HOME'));
-        }
-
-        file_put_contents(env('COMPOSER_HOME')."/auth.json", json_encode([
-            "bitbucket-oauth"=> [],
-            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
-            "gitlab-oauth"=> [],
-            "gitlab-token"=> [],
-            "http-basic"=> [],
-            "bearer"=> []
-        ], JSON_FORCE_OBJECT));
+//        if (! file_exists(env('COMPOSER_HOME'))) {
+//            mkdir(env('COMPOSER_HOME'));
+//        }
+//
+//        file_put_contents(env('COMPOSER_HOME')."/auth.json", json_encode([
+//            "bitbucket-oauth"=> [],
+//            "github-oauth"=> ['github.com' => env('GITHUB_TOKEN')],
+//            "gitlab-oauth"=> [],
+//            "gitlab-token"=> [],
+//            "http-basic"=> [],
+//            "bearer"=> []
+//        ], JSON_FORCE_OBJECT));
     }
 
     protected function execDump($command)
